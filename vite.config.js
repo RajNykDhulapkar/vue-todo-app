d65d7aebad872c98738c06679e6a776c511284bd
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-10-30 16:39:20 +0530

    -a

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,7 @@ console.log(process.env.NODE_ENV == "production");
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    publicPath: process.env.NODE_ENV == "production" ? "/vue-todo-app/" : "/",
+    // publicPath: process.env.NODE_ENV == "production" ? "/vue-todo-app/" : "/dist/",
+    // publicPath: "http://127.0.0.1:5500/dist",
+    base: "./",
 });
