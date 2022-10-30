a1c3bebcf3385bcbe16387be461a5caa96dc0d68
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-10-30 16:24:12 +0530

    -a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV == "production");
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    publicPath: process.env.NODE_ENV === "production" ? "/vue-todo-app/" : "/",
+    publicPath: process.env.NODE_ENV == "production" ? "/vue-todo-app/" : "/",
 });
